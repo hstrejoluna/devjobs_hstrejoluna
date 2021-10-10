3361507de84c0bf0bf2fdbeb5bd2e5229d2bd849
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-10 16:00:34 -0500

    Routes created

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const router = require("./routes");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("It's Alive!");
-});
+app.use("/", router());
 
 app.listen(5000);
