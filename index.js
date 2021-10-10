b9b5f34d82c22ff20fd50028b357c5527e99dda9
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-10 16:07:19 -0500

    handlebars established

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const router = require("./routes");
 
 const app = express();
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "layout",
+  })
+);
+app.set("view engine", "handlebars");
 
 app.use("/", router());
 
