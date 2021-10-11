8c7fb2cb01979a241aa1d92630bc65db6fea2d70
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-10 19:02:30 -0500

    Finishing master page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ exports.showJobs = (req, res) => {
   res.render("home", {
     namePage: "devJobs",
     tagLine: "Find and Post Jobs for Web Developers",
-    bara: true,
+    barra: true,
     boton: true,
   });
 };
