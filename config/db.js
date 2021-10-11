952b4cb7267649d67621b2b1fc0735b1a56e0834
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-10 21:52:57 -0500

    defined vacancies model

--- config/db.js
+++ config/db.js
@@ -6,3 +6,5 @@ mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 mongoose.connection.on("error", (error) => {
   console.log(error);
 });
+
+require("../models/Vacancies");
---
+++ models/Vacancies.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const slug = require("slug");
+const shortid = require("shortid");
+
+const vacanciesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: "The vacancy title is required",
+    trim: true,
+  },
+  Company: {
+    type: String,
+    trim: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: "Location is required",
+  },
+  salary: {
+    type: String,
+    default: 0,
+    trim: true,
+  },
+  contract: {
+    type: String,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  url: {
+    type: String,
+    lowercase: true,
+  },
+  skills: [String],
+  candidates: [
+    {
+      name: String,
+      email: String,
+      cv: String,
+    },
+  ],
+});
+vacanciesSchema.pre("save", function (next) {
+  // creates URL
+  const url = slug(this.title);
+  this.url = `${url}-${shortid.generate()}`;
+  next();
+});
+
+module.exports = mongoose.model("Vacancy", vacanciesSchema);
