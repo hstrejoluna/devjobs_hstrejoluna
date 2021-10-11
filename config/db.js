57d3b8842e633c36cc9c692e7bd827b89c50f1b5
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-10 21:10:18 -0500

    Mongoose Installed and connected to database

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require("mongoose");
+require("./config/db");
+
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 require("dotenv").config({ path: "variables.env" });
 
@@ -18,6 +24,16 @@ app.set("view engine", "handlebars");
 
 // static files
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({mongoUrl: process.env.DATABASE})
+}))
 
 app.use("/", router());
 
