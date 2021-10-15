b420e341b238349f47e6a5e4b8af4d6120e0fd03
hstrejoluna <trejolunatutoriales@gmail.com>
2021-10-14 21:58:50 -0500

    show vacancies on main page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,16 @@
-exports.showJobs = (req, res) => {
+const mongoose = require("mongoose");
+const Vacancy = mongoose.model("Vacancy");
+
+exports.showJobs = async (req, res, next) => {
+  const vacancies = await Vacancy.find().lean();
+
+  if (!vacancies) return next();
+
   res.render("home", {
-    pageName: "devJodbs",
+    pageName: "devJobs",
     tagLine: "Find and Post Jobs for Web Developers",
     barra: true,
     boton: true,
+    vacancies,
   });
 };
