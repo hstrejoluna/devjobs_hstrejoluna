5b25421945751c0e2c6e512c30c12057f47199cc
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-05 17:25:15 -0500

    show vacancies on dashboard admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,28 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
+const Vacancy = mongoose.model("Vacancy");
 
 exports.authUser = passport.authenticate("local", {
-  successRedirect: "/ok",
+  successRedirect: "/admin",
   failureRedirect: "/login",
   failureFlash: true,
 });
 
 exports.verifyUser = (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log("User is authenticated")
+    console.log("User is authenticated");
     return next();
   }
-  console.log("User is not authenticated")
+  console.log("User is not authenticated");
   res.redirect("/login");
 };
 
+exports.showPanel = async (req, res) => {
+  const vacancies = await Vacancy.find({ author: req.user._id });
 
-exports.showPanel = (req, res) => {
   res.render("admin", {
     pageName: "Admin Dashboard",
     tagLine: "Create and Manage your vacancies here",
+    vacancies,
   });
 };
